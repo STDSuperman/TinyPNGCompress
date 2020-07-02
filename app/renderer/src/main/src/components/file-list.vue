8984110859ed323a71878e9a4d37618566f56663
superman <2750556766@qq.com>
2020-07-02 20:54:00 +0800

    新增文件列表组件

---
+++ app/renderer/src/main/src/components/file-list.vue
@@ -0,0 +1,23 @@
+<template>
+   <div class="file-list">
+       <div class="container">
+           
+       </div>
+   </div>
+</template>
+
+<script lang='ts'>
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({})
+export default class FileList extends Vue {
+
+}
+
+</script>
+
+<style lang='scss'>
+.file-list{
+
+}
+</style>
